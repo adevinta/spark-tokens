0c9d83fcb6d3af97d09c4eee2bc23ff2055b764b
solygambas <51904909+solygambas@users.noreply.github.com>
2023-03-15 12:50:44 +0100

    feat: add typography, spacing, border, svg, dark mode

---
+++ build/ios/dist/Color.swift
@@ -0,0 +1,410 @@
+import SwiftUI
+class DarkModeDesignTokens {}
+let bundle = Bundle(for: DarkModeDesignTokens.self)
+extension Color {
+  public static var brandPrimary: Color {
+    return Color.init("brandPrimary", bundle: bundle)
+  }
+  public static var brandOnPrimary: Color {
+    return Color.init("brandOnPrimary", bundle: bundle)
+  }
+  public static var brandPrimaryContainer: Color {
+    return Color.init("brandPrimaryContainer", bundle: bundle)
+  }
+  public static var brandOnPrimaryContainer: Color {
+    return Color.init("brandOnPrimaryContainer", bundle: bundle)
+  }
+  public static var brandPrimaryVariant: Color {
+    return Color.init("brandPrimaryVariant", bundle: bundle)
+  }
+  public static var brandOnPrimaryVariant: Color {
+    return Color.init("brandOnPrimaryVariant", bundle: bundle)
+  }
+  public static var brandSecondary: Color {
+    return Color.init("brandSecondary", bundle: bundle)
+  }
+  public static var brandOnSecondary: Color {
+    return Color.init("brandOnSecondary", bundle: bundle)
+  }
+  public static var brandSecondaryContainer: Color {
+    return Color.init("brandSecondaryContainer", bundle: bundle)
+  }
+  public static var brandOnSecondaryContainer: Color {
+    return Color.init("brandOnSecondaryContainer", bundle: bundle)
+  }
+  public static var brandSecondaryVariant: Color {
+    return Color.init("brandSecondaryVariant", bundle: bundle)
+  }
+  public static var brandOnSecondaryVariant: Color {
+    return Color.init("brandOnSecondaryVariant", bundle: bundle)
+  }
+  public static var brandSuccess: Color {
+    return Color.init("brandSuccess", bundle: bundle)
+  }
+  public static var brandOnSuccess: Color {
+    return Color.init("brandOnSuccess", bundle: bundle)
+  }
+  public static var brandSuccessContainer: Color {
+    return Color.init("brandSuccessContainer", bundle: bundle)
+  }
+  public static var brandOnSuccessContainer: Color {
+    return Color.init("brandOnSuccessContainer", bundle: bundle)
+  }
+  public static var brandAlert: Color {
+    return Color.init("brandAlert", bundle: bundle)
+  }
+  public static var brandOnAlert: Color {
+    return Color.init("brandOnAlert", bundle: bundle)
+  }
+  public static var brandAlertContainer: Color {
+    return Color.init("brandAlertContainer", bundle: bundle)
+  }
+  public static var brandOnAlertContainer: Color {
+    return Color.init("brandOnAlertContainer", bundle: bundle)
+  }
+  public static var brandError: Color {
+    return Color.init("brandError", bundle: bundle)
+  }
+  public static var brandOnError: Color {
+    return Color.init("brandOnError", bundle: bundle)
+  }
+  public static var brandErrorContainer: Color {
+    return Color.init("brandErrorContainer", bundle: bundle)
+  }
+  public static var brandOnErrorContainer: Color {
+    return Color.init("brandOnErrorContainer", bundle: bundle)
+  }
+  public static var brandInfo: Color {
+    return Color.init("brandInfo", bundle: bundle)
+  }
+  public static var brandOnInfo: Color {
+    return Color.init("brandOnInfo", bundle: bundle)
+  }
+  public static var brandInfoContainer: Color {
+    return Color.init("brandInfoContainer", bundle: bundle)
+  }
+  public static var brandOnInfoContainer: Color {
+    return Color.init("brandOnInfoContainer", bundle: bundle)
+  }
+  public static var brandNeutral: Color {
+    return Color.init("brandNeutral", bundle: bundle)
+  }
+  public static var brandOnNeutral: Color {
+    return Color.init("brandOnNeutral", bundle: bundle)
+  }
+  public static var brandNeutralContainer: Color {
+    return Color.init("brandNeutralContainer", bundle: bundle)
+  }
+  public static var brandOnNeutralContainer: Color {
+    return Color.init("brandOnNeutralContainer", bundle: bundle)
+  }
+  public static var brandBackground: Color {
+    return Color.init("brandBackground", bundle: bundle)
+  }
+  public static var brandOnBackground: Color {
+    return Color.init("brandOnBackground", bundle: bundle)
+  }
+  public static var brandSurface: Color {
+    return Color.init("brandSurface", bundle: bundle)
+  }
+  public static var brandOnSurface: Color {
+    return Color.init("brandOnSurface", bundle: bundle)
+  }
+  public static var brandSurfaceInverse: Color {
+    return Color.init("brandSurfaceInverse", bundle: bundle)
+  }
+  public static var brandOnSurfaceInverse: Color {
+    return Color.init("brandOnSurfaceInverse", bundle: bundle)
+  }
+  public static var brandOutline: Color {
+    return Color.init("brandOutline", bundle: bundle)
+  }
+  public static var brandOverlay: Color {
+    return Color.init("brandOverlay", bundle: bundle)
+  }
+  public static var brandOnSurfaceDim1: Color {
+    return Color.init("brandOnSurfaceDim1", bundle: bundle)
+  }
+  public static var brandOnSurfaceDim2: Color {
+    return Color.init("brandOnSurfaceDim2", bundle: bundle)
+  }
+  public static var brandOnSurfaceDim3: Color {
+    return Color.init("brandOnSurfaceDim3", bundle: bundle)
+  }
+  public static var brandOnSurfaceDim4: Color {
+    return Color.init("brandOnSurfaceDim4", bundle: bundle)
+  }
+  public static var brandOnSurfaceDim5: Color {
+    return Color.init("brandOnSurfaceDim5", bundle: bundle)
+  }
+  public static var brandPrimaryDim1: Color {
+    return Color.init("brandPrimaryDim1", bundle: bundle)
+  }
+  public static var brandPrimaryDim2: Color {
+    return Color.init("brandPrimaryDim2", bundle: bundle)
+  }
+  public static var brandPrimaryDim3: Color {
+    return Color.init("brandPrimaryDim3", bundle: bundle)
+  }
+  public static var brandPrimaryDim4: Color {
+    return Color.init("brandPrimaryDim4", bundle: bundle)
+  }
+  public static var brandPrimaryDim5: Color {
+    return Color.init("brandPrimaryDim5", bundle: bundle)
+  }
+  public static var coreTheblue50: Color {
+    return Color.init("coreTheblue50", bundle: bundle)
+  }
+  public static var coreTheblue100: Color {
+    return Color.init("coreTheblue100", bundle: bundle)
+  }
+  public static var coreTheblue200: Color {
+    return Color.init("coreTheblue200", bundle: bundle)
+  }
+  public static var coreTheblue300: Color {
+    return Color.init("coreTheblue300", bundle: bundle)
+  }
+  public static var coreTheblue400: Color {
+    return Color.init("coreTheblue400", bundle: bundle)
+  }
+  public static var coreTheblue500: Color {
+    return Color.init("coreTheblue500", bundle: bundle)
+  }
+  public static var coreTheblue600: Color {
+    return Color.init("coreTheblue600", bundle: bundle)
+  }
+  public static var coreTheblue700: Color {
+    return Color.init("coreTheblue700", bundle: bundle)
+  }
+  public static var coreTheblue800: Color {
+    return Color.init("coreTheblue800", bundle: bundle)
+  }
+  public static var coreTheblue900: Color {
+    return Color.init("coreTheblue900", bundle: bundle)
+  }
+  public static var coreTheblueV: Color {
+    return Color.init("coreTheblueV", bundle: bundle)
+  }
+  public static var coreSugarcotton50: Color {
+    return Color.init("coreSugarcotton50", bundle: bundle)
+  }
+  public static var coreSugarcotton100: Color {
+    return Color.init("coreSugarcotton100", bundle: bundle)
+  }
+  public static var coreSugarcotton200: Color {
+    return Color.init("coreSugarcotton200", bundle: bundle)
+  }
+  public static var coreSugarcotton300: Color {
+    return Color.init("coreSugarcotton300", bundle: bundle)
+  }
+  public static var coreSugarcotton400: Color {
+    return Color.init("coreSugarcotton400", bundle: bundle)
+  }
+  public static var coreSugarcotton500: Color {
+    return Color.init("coreSugarcotton500", bundle: bundle)
+  }
+  public static var coreSugarcotton600: Color {
+    return Color.init("coreSugarcotton600", bundle: bundle)
+  }
+  public static var coreSugarcotton700: Color {
+    return Color.init("coreSugarcotton700", bundle: bundle)
+  }
+  public static var coreSugarcotton800: Color {
+    return Color.init("coreSugarcotton800", bundle: bundle)
+  }
+  public static var coreSugarcotton900: Color {
+    return Color.init("coreSugarcotton900", bundle: bundle)
+  }
+  public static var coreApple50: Color {
+    return Color.init("coreApple50", bundle: bundle)
+  }
+  public static var coreApple100: Color {
+    return Color.init("coreApple100", bundle: bundle)
+  }
+  public static var coreApple200: Color {
+    return Color.init("coreApple200", bundle: bundle)
+  }
+  public static var coreApple300: Color {
+    return Color.init("coreApple300", bundle: bundle)
+  }
+  public static var coreApple400: Color {
+    return Color.init("coreApple400", bundle: bundle)
+  }
+  public static var coreApple500: Color {
+    return Color.init("coreApple500", bundle: bundle)
+  }
+  public static var coreApple600: Color {
+    return Color.init("coreApple600", bundle: bundle)
+  }
+  public static var coreApple700: Color {
+    return Color.init("coreApple700", bundle: bundle)
+  }
+  public static var coreApple800: Color {
+    return Color.init("coreApple800", bundle: bundle)
+  }
+  public static var coreApple900: Color {
+    return Color.init("coreApple900", bundle: bundle)
+  }
+  public static var coreWiggins50: Color {
+    return Color.init("coreWiggins50", bundle: bundle)
+  }
+  public static var coreWiggins100: Color {
+    return Color.init("coreWiggins100", bundle: bundle)
+  }
+  public static var coreWiggins200: Color {
+    return Color.init("coreWiggins200", bundle: bundle)
+  }
+  public static var coreWiggins300: Color {
+    return Color.init("coreWiggins300", bundle: bundle)
+  }
+  public static var coreWiggins400: Color {
+    return Color.init("coreWiggins400", bundle: bundle)
+  }
+  public static var coreWiggins500: Color {
+    return Color.init("coreWiggins500", bundle: bundle)
+  }
+  public static var coreWiggins600: Color {
+    return Color.init("coreWiggins600", bundle: bundle)
+  }
+  public static var coreWiggins700: Color {
+    return Color.init("coreWiggins700", bundle: bundle)
+  }
+  public static var coreWiggins800: Color {
+    return Color.init("coreWiggins800", bundle: bundle)
+  }
+  public static var coreWiggins900: Color {
+    return Color.init("coreWiggins900", bundle: bundle)
+  }
+  public static var coreChili50: Color {
+    return Color.init("coreChili50", bundle: bundle)
+  }
+  public static var coreChili100: Color {
+    return Color.init("coreChili100", bundle: bundle)
+  }
+  public static var coreChili200: Color {
+    return Color.init("coreChili200", bundle: bundle)
+  }
+  public static var coreChili300: Color {
+    return Color.init("coreChili300", bundle: bundle)
+  }
+  public static var coreChili400: Color {
+    return Color.init("coreChili400", bundle: bundle)
+  }
+  public static var coreChili500: Color {
+    return Color.init("coreChili500", bundle: bundle)
+  }
+  public static var coreChili600: Color {
+    return Color.init("coreChili600", bundle: bundle)
+  }
+  public static var coreChili700: Color {
+    return Color.init("coreChili700", bundle: bundle)
+  }
+  public static var coreChili800: Color {
+    return Color.init("coreChili800", bundle: bundle)
+  }
+  public static var coreChili900: Color {
+    return Color.init("coreChili900", bundle: bundle)
+  }
+  public static var coreSky50: Color {
+    return Color.init("coreSky50", bundle: bundle)
+  }
+  public static var coreSky100: Color {
+    return Color.init("coreSky100", bundle: bundle)
+  }
+  public static var coreSky200: Color {
+    return Color.init("coreSky200", bundle: bundle)
+  }
+  public static var coreSky300: Color {
+    return Color.init("coreSky300", bundle: bundle)
+  }
+  public static var coreSky400: Color {
+    return Color.init("coreSky400", bundle: bundle)
+  }
+  public static var coreSky500: Color {
+    return Color.init("coreSky500", bundle: bundle)
+  }
+  public static var coreSky600: Color {
+    return Color.init("coreSky600", bundle: bundle)
+  }
+  public static var coreSky700: Color {
+    return Color.init("coreSky700", bundle: bundle)
+  }
+  public static var coreSky800: Color {
+    return Color.init("coreSky800", bundle: bundle)
+  }
+  public static var coreSky900: Color {
+    return Color.init("coreSky900", bundle: bundle)
+  }
+  public static var coreSurfer50: Color {
+    return Color.init("coreSurfer50", bundle: bundle)
+  }
+  public static var coreSurfer100: Color {
+    return Color.init("coreSurfer100", bundle: bundle)
+  }
+  public static var coreSurfer200: Color {
+    return Color.init("coreSurfer200", bundle: bundle)
+  }
+  public static var coreSurfer300: Color {
+    return Color.init("coreSurfer300", bundle: bundle)
+  }
+  public static var coreSurfer400: Color {
+    return Color.init("coreSurfer400", bundle: bundle)
+  }
+  public static var coreSurfer500: Color {
+    return Color.init("coreSurfer500", bundle: bundle)
+  }
+  public static var coreSurfer600: Color {
+    return Color.init("coreSurfer600", bundle: bundle)
+  }
+  public static var coreSurfer700: Color {
+    return Color.init("coreSurfer700", bundle: bundle)
+  }
+  public static var coreSurfer800: Color {
+    return Color.init("coreSurfer800", bundle: bundle)
+  }
+  public static var coreSurfer900: Color {
+    return Color.init("coreSurfer900", bundle: bundle)
+  }
+  public static var coreKiwi50: Color {
+    return Color.init("coreKiwi50", bundle: bundle)
+  }
+  public static var coreKiwi100: Color {
+    return Color.init("coreKiwi100", bundle: bundle)
+  }
+  public static var coreKiwi200: Color {
+    return Color.init("coreKiwi200", bundle: bundle)
+  }
+  public static var coreKiwi300: Color {
+    return Color.init("coreKiwi300", bundle: bundle)
+  }
+  public static var coreKiwi400: Color {
+    return Color.init("coreKiwi400", bundle: bundle)
+  }
+  public static var coreKiwi500: Color {
+    return Color.init("coreKiwi500", bundle: bundle)
+  }
+  public static var coreKiwi600: Color {
+    return Color.init("coreKiwi600", bundle: bundle)
+  }
+  public static var coreKiwi700: Color {
+    return Color.init("coreKiwi700", bundle: bundle)
+  }
+  public static var coreKiwi800: Color {
+    return Color.init("coreKiwi800", bundle: bundle)
+  }
+  public static var coreKiwi900: Color {
+    return Color.init("coreKiwi900", bundle: bundle)
+  }
+  public static var coreBlack: Color {
+    return Color.init("coreBlack", bundle: bundle)
+  }
+  public static var coreWhite: Color {
+    return Color.init("coreWhite", bundle: bundle)
+  }
+  public static var coreFacebook: Color {
+    return Color.init("coreFacebook", bundle: bundle)
+  }
+  public static var coreTwitter: Color {
+    return Color.init("coreTwitter", bundle: bundle)
+  }
+}
