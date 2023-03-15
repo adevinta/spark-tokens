62f26c14851bde341b922c3ae99e1e6bc099902e
solygambas <51904909+solygambas@users.noreply.github.com>
2023-03-15 16:30:28 +0100

    feat: add web demo

---
+++ build/web/demo/.eleventy.js
@@ -0,0 +1,20 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+  // Copy the `img` and `css` folders to the output
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy({
+    "node_modules/spark-tokens/build/web/dist/variables.css":
+      "css/variables.css",
+    "node_modules/spark-tokens/build/web/dist/variables-dark.css":
+      "css/variables-dark.css",
+    "node_modules/spark-tokens/build/web/dist/variables-hc.css":
+      "css/variables-hc.css",
+    "node_modules/spark-tokens/build/web/dist/variables-hc-dark.css":
+      "css/variables-hc-dark.css",
+    "node_modules/spark-tokens/build/web/dist/images": "images",
+  });
+};
