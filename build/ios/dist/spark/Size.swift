95f4767ed1e9d3c4c480e686cb5f34cc2282050b
solygambas <51904909+solygambas@users.noreply.github.com>
2023-04-07 16:42:57 +0200

    feat: add multibrand

---
+++ build/ios/dist/spark/Size.swift
@@ -0,0 +1,63 @@
+
+//
+// Size.swift
+//
+
+// Do not edit directly
+// Generated on Fri, 07 Apr 2023 14:34:56 GMT
+
+
+import UIKit
+
+public class Size {
+    public static let badgeBorderRadius = CGFloat(8.00)
+    public static let badgeFontSize = CGFloat(16.00)
+    public static let badgePaddingHorizontal = CGFloat(8.00)
+    public static let badgePaddingVertical = CGFloat(4.00)
+    public static let borderRadiusLg = CGFloat(16.00)
+    public static let borderRadiusMd = CGFloat(8.00)
+    public static let borderRadiusNone = CGFloat(0.00)
+    public static let borderRadiusSm = CGFloat(4.00)
+    public static let borderRadiusXl = CGFloat(32.00)
+    public static let borderWidthMd = CGFloat(2.00)
+    public static let borderWidthNone = CGFloat(0.00)
+    public static let borderWidthSm = CGFloat(1.00)
+    public static let buttonBorderRadius = CGFloat(16.00)
+    public static let buttonFontSize = CGFloat(16.00)
+    public static let buttonPadding = CGFloat(8.00)
+    public static let buttonSecondaryBorderWidth = CGFloat(2.00)
+    public static let fontActionBody1Link = CGFloat(16.00)
+    public static let fontActionBody1LinkHighlight = CGFloat(16.00)
+    public static let fontActionBody2Link = CGFloat(14.00)
+    public static let fontActionBody2LinkHighlight = CGFloat(14.00)
+    public static let fontActionCallout = CGFloat(16.00)
+    public static let fontActionCaptionLink = CGFloat(12.00)
+    public static let fontActionCaptionLinkHighlight = CGFloat(12.00)
+    public static let fontContentBody1 = CGFloat(16.00)
+    public static let fontContentBody1Highlight = CGFloat(16.00)
+    public static let fontContentBody2 = CGFloat(14.00)
+    public static let fontContentBody2Highlight = CGFloat(14.00)
+    public static let fontContentCaption = CGFloat(12.00)
+    public static let fontContentCaptionHighlight = CGFloat(12.00)
+    public static let fontContentSmall = CGFloat(10.00)
+    public static let fontContentSmallHighlight = CGFloat(10.00)
+    public static let fontTitleDisplay1 = CGFloat(40.00)
+    public static let fontTitleDisplay1Expanded = CGFloat(48.00)
+    public static let fontTitleDisplay2 = CGFloat(32.00)
+    public static let fontTitleDisplay2Expanded = CGFloat(40.00)
+    public static let fontTitleDisplay3 = CGFloat(24.00)
+    public static let fontTitleDisplay3Expanded = CGFloat(32.00)
+    public static let fontTitleHeadline1 = CGFloat(20.00)
+    public static let fontTitleHeadline1Expanded = CGFloat(24.00)
+    public static let fontTitleHeadline2 = CGFloat(18.00)
+    public static let fontTitleHeadline2Expanded = CGFloat(20.00)
+    public static let fontTitleSubhead = CGFloat(16.00)
+    public static let fontTitleSubheadExpanded = CGFloat(16.00)
+    public static let padding2xl = CGFloat(32.00)
+    public static let padding3xl = CGFloat(40.00)
+    public static let paddingLg = CGFloat(16.00)
+    public static let paddingMd = CGFloat(8.00)
+    public static let paddingNone = CGFloat(0.00)
+    public static let paddingSm = CGFloat(4.00)
+    public static let paddingXl = CGFloat(24.00)
+}
