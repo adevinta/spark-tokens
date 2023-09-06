0350f7fd7290f4197f654b79c644024d78265b8a
solygambas <51904909+solygambas@users.noreply.github.com>
2023-09-06 12:04:05 +0200

    fix: web demo

--- build/web/demo/_data/color.js
+++ build/web/demo/_data/color.js
@@ -3,8 +3,8 @@ const tokens = require("spark-tokens/build/web/dist/spark/tokens.json");
 // Hard-coding which tokens to show in the doc site
 // just so that the line up (appear in the same order) and are grouped nicely
 const colors = [
-  "primary-primary",
-  "secondary-secondary",
+  "main-main",
+  "support-support",
   "feedback-success",
   "feedback-alert",
   "feedback-error",
